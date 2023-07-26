01f4c3c7760e8b24682786c7e1755a25e83b050f
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-07-26 11:32:14 +0300

    Added houses

--- base/models.py
+++ base/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Apartment(models.Model):
+    name = models.CharField(max_length=200, null=True, blank=True)
+    address = models.CharField(max_length=200, null=True, blank=True)
+    city = models.CharField(max_length=200, null=True, blank=True)
+    county = models.CharField(max_length=200, null=True, blank=True)
+    region = models.CharField(max_length=200, null=True, blank=True)
+    photo = models.ImageField(null=True, blank=True, default='default.jpg')
+    photo_1 = models.ImageField(null=True, blank=True, default='default.jpg')
+    photo_2 = models.ImageField(null=True, blank=True, default='default.jpg')
+    # video = models.FileField(null=True, blank=True, default='default.mp4')
+    bedrooms = models.IntegerField(null=True, blank=True)
+
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self):
+        return self.name
--- base/views.py
+++ base/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Apartment
 
 # Create your views here.
 
 def landing_page(request):
     return render(request, 'base/landing_page.html')
+
+def home(request):
+    apartments = Apartment.objects.all()
+    print(apartments)
+    context = {'apartments':apartments}
+    return render(request, 'base/home.html', context)
